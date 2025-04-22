a4e0f8efc9ef0fd182542fea919d188afedda535
MrCrowNFT <mrcrownft@gmail.com>
2025-04-22 06:07:16 -0400

    app change: remove PORT

--- backend/app.js
+++ backend/app.js
@@ -9,13 +9,12 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5500;
 const io = new Server(server, {
   cors: { origin: "*" }, // Allows cross-origin requests for the frontend
 });
 
 app.use(cors());
-server.listen(PORT, () => {
+server.listen(() => {
   console.log("Database connected");
   console.log(`Server running on port ${PORT}`);
 });
