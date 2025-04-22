dda807b09491d85787227eb60df2b2e5f7fb996f
MrCrowNFT <mrcrownft@gmail.com>
2025-04-22 08:19:31 -0400

    auth router

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
 import db from "./db/db";
+import authRouter from "./routes/auth.routes";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" }, // Allows cross-origin requests for the frontend
 });
+
+app.use("/auth", authRouter);
 
 app.use(cors());
 server.listen(() => {
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,12 +5,13 @@ import {
   refreshAccessToken,
   signup,
 } from "../controllers/auth.controller";
+import { authenticate } from "../middleware/auth.middleware";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
 authRouter.post("/refresh", refreshAccessToken);
-authRouter.post("/logout", logout);
+authRouter.post("/logout", authenticate, logout);
 
 export default authRouter;
