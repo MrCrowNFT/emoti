b16a369fa17b6bbd460f4073c6904d8e673099e9
MrCrowNFT <mrcrownft@gmail.com>
2025-04-23 06:44:25 -0400

    socket io server start

--- backend/app.js
+++ backend/app.js
@@ -10,14 +10,29 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+const PORT = process.env.PORT || 5500;
+
 const io = new Server(server, {
   cors: { origin: "*" }, // Allows cross-origin requests for the frontend
 });
 
+app.use(cors());
 app.use("/auth", authRouter);
 
-app.use(cors());
-server.listen(() => {
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("Message", (data) => {
+    console.log("Message received:", data);
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(PORT, () => {
   console.log("Database connected");
   console.log(`Server running on port ${PORT}`);
 });
